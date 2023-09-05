04df0858e3b23c2ff218c100358b78e12c0396b0
leonardoserra <serra.leonardo92@gmail.com>
2023-09-05 15:59:09 +0200

    creato controllo pari dispari

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Ecco la lista dei miei cibi preferiti: ");
 
 // creo array di stringhe coi cibi preferiti
-string[] favouriteFood = { "Pizza", "Lasagne", "Orata", "Filetto al pepe verde", "Caprese", "Carbonara", "Pollo Arrosto", "Ramen", "Kebab" };
+string[] favouriteFood = { "Pizza", "Lasagne", "Filetto al pepe verde", "Caprese", "Carbonara", "Pollo Arrosto", "Ramen", "Kebab" };
 
 //Stampo la lunghezza dell'array
 Console.WriteLine("\nAttualmente nella lista ci sono " + favouriteFood.Length + " pietanze.");
@@ -18,8 +18,18 @@ Console.WriteLine("\nIl mio cibo preferito é " + favouriteFood[0]+ "! Buonissim
 Console.WriteLine("\nL'ultima pietanza della lista é " + favouriteFood[favouriteFood.Length - 1] + "! Buono lo stesso :)");
 
 
-//bonus : stampo la pietanza mediana
-Console.WriteLine("\nBONUS: Alla posizione centrale abbiamo: " + favouriteFood[(favouriteFood.Length/2) - 1] + ". Una bontá!");
+//bonus : stampo la pietanza mediana, controllo se lista pari o dispari con output differente
+if(favouriteFood.Length % 2 == 0){
+
+    Console.WriteLine("\nBONUS: Alla posizione centrale abbiamo 2 pietanze dal momento che la lista contiene un numero pari di elementi: ");
+    Console.WriteLine("\nPos." + ((favouriteFood.Length / 2)) +": " + favouriteFood[(favouriteFood.Length / 2) - 1] + ". Una bontá!");
+    Console.WriteLine("Pos." + ((favouriteFood.Length / 2) + 1) + ": " + favouriteFood[(favouriteFood.Length / 2)] + ". Una bontá!");
+}
+else
+{
+    Console.WriteLine("\nBONUS: Alla posizione centrale abbiamo 1 pietanza dal momento che la lista contiene un numero dispari di elementi: ");
+    Console.WriteLine("Pos." + ((favouriteFood.Length / 2) + 1) + ": " + favouriteFood[(favouriteFood.Length/2)] + ". Una bontá!");
+}
 
 
 Console.WriteLine("\nMade by Leonardo Serra classe 07");
