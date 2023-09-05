626ae2aa759c64825d6fe8f977de26b7b6ba7421
leonardoserra <serra.leonardo92@gmail.com>
2023-09-05 14:39:43 +0200

    Esercizio base completato

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Prova di Commit");
+Console.WriteLine("Ecco la lista dei miei cibi preferiti: ");
+// creo array di stringhe coi cibi preferiti
+string[] favouriteFood = { "Pizza", "Lasagne", "Orata", "Filetto al pepe verde", "Caprese", "Carbonara", "Ramen", "Kebab" };
+Console.WriteLine("Attualmente nella lista ci sono " + favouriteFood.Length + " pietanze.");
+
+for (int i = 0; i < favouriteFood.Length; i++)
+{
+    Console.WriteLine("Posizione n." + (i + 1) +  " " + favouriteFood[i]);
+}
+
+Console.WriteLine("Il mio cibo preferito é " + favouriteFood[0]+ "! Buono!");
+Console.WriteLine("L'ultima pietanza della lista é " + favouriteFood[favouriteFood.Length - 1] + "! Buono lo stesso :)");
