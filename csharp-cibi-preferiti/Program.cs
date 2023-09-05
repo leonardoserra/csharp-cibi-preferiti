19d9e026fa261892c34d9ea11c46e0aeabd9a876
leonardoserra <serra.leonardo92@gmail.com>
2023-09-05 16:23:06 +0200

    refactory da \n a newLine

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Ecco la lista dei miei cibi preferiti: ");
-
+string newLine = Environment.NewLine;
 // creo array di stringhe coi cibi preferiti
 string[] favouriteFood = { "Pizza", "Lasagne", "Filetto al pepe verde", "Orata", "Caprese", "Carbonara", "Pollo Arrosto", "Ramen", "Kebab" };
 
 //Stampo la lunghezza dell'array
-Console.WriteLine($"\nAttualmente nella lista ci sono { favouriteFood.Length} pietanze.");
+Console.WriteLine($"{newLine}Attualmente nella lista ci sono { favouriteFood.Length} pietanze.");
 
 //ciclo l'array per stampare la lista
 for (int i = 0; i < favouriteFood.Length; i++)
@@ -14,22 +14,22 @@ for (int i = 0; i < favouriteFood.Length; i++)
 }
 
 //stampo la primo e l'ultima pietanza
-Console.WriteLine($"\nIl mio cibo preferito é { favouriteFood[0]}! Buonissima!");
-Console.WriteLine($"\nL'ultima pietanza della lista é {favouriteFood[favouriteFood.Length - 1]}! Buono lo stesso :)");
+Console.WriteLine($"{newLine}Il mio cibo preferito é { favouriteFood[0]}! Buonissima!");
+Console.WriteLine($"{newLine}L'ultima pietanza della lista é {favouriteFood[favouriteFood.Length - 1]}! Buono lo stesso :)");
 
 
 //bonus : stampo la pietanza mediana, controllo se lista pari o dispari con output differente
 if(favouriteFood.Length % 2 == 0){
 
-    Console.WriteLine("\nBONUS: Alla posizione centrale abbiamo 2 pietanze dal momento che la lista contiene un numero pari di elementi: ");
-    Console.WriteLine($"\nPos.{((favouriteFood.Length / 2))}:{favouriteFood[(favouriteFood.Length / 2) - 1]}. Una bontá!");
+    Console.WriteLine($"{newLine}BONUS: Alla posizione centrale abbiamo 2 pietanze dal momento che la lista contiene un numero pari di elementi: ");
+    Console.WriteLine($"{newLine}Pos.{((favouriteFood.Length / 2))}:{favouriteFood[(favouriteFood.Length / 2) - 1]}. Una bontá!");
     Console.WriteLine($"Pos.{((favouriteFood.Length / 2) + 1)}:{favouriteFood[(favouriteFood.Length / 2)]}. Yum!");
 }
 else
 {
-    Console.WriteLine("\nBONUS: Alla posizione centrale abbiamo 1 pietanza dal momento che la lista contiene un numero dispari di elementi: ");
+    Console.WriteLine($"{newLine}BONUS: Alla posizione centrale abbiamo 1 pietanza dal momento che la lista contiene un numero dispari di elementi: ");
     Console.WriteLine($"Pos.{((favouriteFood.Length / 2) + 1)}:{favouriteFood[(favouriteFood.Length/2)]}. Una bontá!");
 }
 
 
-Console.WriteLine("\nMade by Leonardo Serra classe 07");
+Console.WriteLine($"{newLine}Made by Leonardo Serra classe 07");
